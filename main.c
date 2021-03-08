965dac938b1fcc11f6a85ce1eb586fac09232fc8
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-03-08 14:55:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Gehört zum Aufgabenteil 14
 void myPrint(char *string) {
 	printf("\n%s\n", string);
 }
 
 int main()
 {
-	// 1
 	int a = 17;
 	int b = -6;
 	int c;
 
-	// 2
 	int *pointerA;
-	int *pointerB;	// Alternative Schreibweise (wird "korrigiert" durch Auto-Formatierung): int* pointerB;
+	int* pointerB;
 
-	// 3
 	pointerA = &a;
 	pointerB = &b;
 
-	// 4
 	printf("\nAddress of a: %p", &a);
 	printf("\nAddress of pointerA: %p", &pointerA);
 	printf("\nValue of a: %d", a);
@@ -32,38 +27,32 @@ int main()
 	printf("\nValue of b: %d", b);
 	printf("\nValue of pointerB: %d\n", *pointerB);
 
-	// 5
 	int *pointerC = &c;
 
-	// 6
-	// Erwartung: Variable c ist nicht initialisiert und enthält einen zufälligen (bzw. unsinnigen) Wert.
+	// Variable c ist nicht initialisiert und enthält einen zufälligen Wert.
 	printf("\nAddress of c: %p", c);
 	printf("\nAddress of pointerC: %p", &pointerC);
 	printf("\nValue of c: %d", c);
 	printf("\nValue of pointerC: %d\n", *pointerC);
 
-	// 7
 	char character = 'x';
 
-	// 8
 	char *pointer1 = &character;
 	char *pointer2 = pointer1;
 
-	// 9
 	*pointer1 = 'X';
 	printf("\nInhalt von pointer2: %c\n", *pointer2);
 
-	// 10
 	int array[] = {1, 2, 3, 5, 7};
 
-	// 11a (ohne Zeiger)
+	// ohne Zeiger
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
 		printf("\nWert %d: %d", i, array[i]);
 	}
 	printf("\n");
 
-	// 11b (mit Zeiger)
+	// mit Zeiger
 	int *arrayIndex = array;
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
@@ -73,7 +62,7 @@ int main()
 	}
 	printf("\n");
 
-	// 12a (ohne Zeiger)
+	// ohne Zeiger
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
 		printf("\nEintrag %d:", i);
@@ -82,7 +71,7 @@ int main()
 	}
 	printf("\n");
 
-	// 12b (mit Zeiger)
+	// mit Zeiger
 	arrayIndex = array;
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
@@ -93,12 +82,9 @@ int main()
 	}
 	printf("\n");
 
-	// 13
 	char name[] = "Ioannis Christodoulakis";
 
-	// 14
 	myPrint(name);
-
 
 	return EXIT_SUCCESS;
 }
