eee377bf193cdb85d09587afecca9d8d09b452d7
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-03-23 12:21:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Task 14
 void myPrint(char *string) {
 	printf("\n%s\n", string);
 }
 
 int main()
 {
+	// Task 1
 	int a = 17;
 	int b = -6;
 	int c;
 
+	// Task 2
 	int *pointerA;
 	int* pointerB;
 
+	// Task 3
 	pointerA = &a;
 	pointerB = &b;
 
+	// Task 4
 	printf("\nAddress of a: %p", &a);
 	printf("\nAddress of pointerA: %p", &pointerA);
 	printf("\nValue of a: %d", a);
@@ -27,32 +32,40 @@ int main()
 	printf("\nValue of b: %d", b);
 	printf("\nValue of pointerB: %d\n", *pointerB);
 
+	// Task 5
 	int *pointerC = &c;
 
-	// Variable c ist nicht initialisiert und enthält einen zufälligen Wert.
+	// Task 6
+	// Variable c is not initialised and contains a random value.
 	printf("\nAddress of c: %p", c);
 	printf("\nAddress of pointerC: %p", &pointerC);
 	printf("\nValue of c: %d", c);
 	printf("\nValue of pointerC: %d\n", *pointerC);
 
+	// Task 7
 	char character = 'x';
 
+	// Task 8
 	char *pointer1 = &character;
 	char *pointer2 = pointer1;
 
+	// Task 9
 	*pointer1 = 'X';
 	printf("\nInhalt von pointer2: %c\n", *pointer2);
-
+	
+	// Task 10
 	int array[] = {1, 2, 3, 5, 7};
 
-	// ohne Zeiger
+	// Task 11a
+	// without Pointer
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
 		printf("\nWert %d: %d", i, array[i]);
 	}
 	printf("\n");
 
-	// mit Zeiger
+	// Task 11b
+	// with Pointer
 	int *arrayIndex = array;
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
@@ -62,7 +75,8 @@ int main()
 	}
 	printf("\n");
 
-	// ohne Zeiger
+	// Task 12a
+	// without Pointer
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
 		printf("\nEintrag %d:", i);
@@ -71,7 +85,8 @@ int main()
 	}
 	printf("\n");
 
-	// mit Zeiger
+	// Task 12b
+	// with Pointer
 	arrayIndex = array;
 	for (int i = 0; i < sizeof(array) / sizeof(int); i++)
 	{
@@ -82,8 +97,10 @@ int main()
 	}
 	printf("\n");
 
+	// Task 13
 	char name[] = "Ioannis Christodoulakis";
 
+	// Task 14
 	myPrint(name);
 
 	return EXIT_SUCCESS;
